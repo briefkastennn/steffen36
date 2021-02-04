7a84a75d0da9e4fc6c7c49ae388b4287b746fd5b
briefkastennn <78443939+briefkastennn@users.noreply.github.com>
2021-02-04 09:45:46 +0100

    Create MANOLO1.tsx

---
+++ tiles_png/steffenpngs/MANOLO1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="MANOLO1" tilewidth="32" tileheight="32" tilecount="544" columns="32">
+ <image source="../../../GitHub/Steffen36/steffen36/tiles_png/steffenpngs/Manolo1.png" width="1024" height="552"/>
+</tileset>
